d0bf685dc58258d31b103653e1a10049c72cc40a
backurdi <bachir098@gmail.com>
2021-01-01 15:46:35 +0100

    testing forms

--- src/components/paymentForm.vue
+++ src/components/paymentForm.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="payment-form-container">
-    <form name="payment-form" netlify>
-      <input type="text" placeholder="name">
-      <input type="submit" placeholder="submit">
-    </form>
+    <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
     <!--<div ref="dropinContainer" id="dropin-container"></div>-->
   </div>
 </template>
